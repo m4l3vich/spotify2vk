7b46f81b41f9adaa12520f55889aa1af191994b5
Unknown <m4l3vich@mail.ru>
2017-12-07 22:14:33 +0800

    Standardized code

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var connect = require('spotify-local-control')
 var https = require('https')
 var querystring = require('querystring')
 var client = connect()
-var argv = require('minimist')(process.argv.slice(2)) || {};
-var prevText = "";
-var isExiting = false;
-var isPaused = false;
+var argv = require('minimist')(process.argv.slice(2)) || {}
+var prevText = ''
+var isExiting = false
+var isPaused = false
 console.log(`Starting Spotify2VK v${require('./package.json').version} by M4L3VICH`)
 
 var config = {
@@ -15,7 +15,7 @@ var config = {
   current: argv.current
 }
 
-if(argv.help || argv.h){
+if (argv.help || argv.h) {
   console.log([
     `Spotify2VK: show Spotify currently playing track in VK status`,
     `Usage:`,
@@ -25,33 +25,33 @@ if(argv.help || argv.h){
     `--current              Use current (before running app) status as replacer`
   ].join('\n'))
   process.exit()
-}else if(!config.token){
+} else if (!config.token) {
   console.error('Please provide a valid VK access_token by adding "--token <access_token>"')
   process.exit()
 }
 
-async function loop(){
+async function loop () {
   // console.log('previous',prevText)
-  var status = await client.status();
-  var track = status.body.track;
+  var status = await client.status()
+  var track = status.body.track
   var text = `${track.artist_resource.name} - ${track.track_resource.name}`
 
-  if(prevText != text && status.body.playing){
+  if (prevText !== text && status.body.playing) {
     prevText = text
     isPaused = false
-    https.get(`https://api.vk.com/method/status.set?text=${querystring.escape(config.statusPrefix.trim()+" "+text)}&access_token=${config.token}`, function(res){
+    https.get(`https://api.vk.com/method/status.set?text=${querystring.escape(config.statusPrefix.trim() + ' ' + text)}&access_token=${config.token}`, function (res) {
       res.on('data', (d) => {
         var r = JSON.parse(d.toString())
-        if(r.error){
-          console.error('VK error:',r.error.error_msg)
+        if (r.error) {
+          console.error('VK error:', r.error.error_msg)
           process.exit()
         }
       })
     })
-    console.log('New track:',text)
-  }else if(!status.body.playing && !isPaused){
+    console.log('New track:', text)
+  } else if (!status.body.playing && !isPaused) {
     console.log('Paused, setting replacer status')
-    https.get(`https://api.vk.com/method/status.set?text=${querystring.escape(config.statusReplace)}&access_token=${config.token}`, function(res){
+    https.get(`https://api.vk.com/method/status.set?text=${querystring.escape(config.statusReplace)}&access_token=${config.token}`, function (res) {
       res.on('data', (d) => {
         console.log('Done')
         isPaused = true
@@ -62,36 +62,36 @@ async function loop(){
   setTimeout(loop, 500)
 }
 
-if(config.current){
+if (config.current) {
   console.log('Getting current user status...')
-  https.get(`https://api.vk.com/method/status.get?access_token=${config.token}`, function(res){
+  https.get(`https://api.vk.com/method/status.get?access_token=${config.token}`, function (res) {
     res.on('data', (d) => {
       var r = JSON.parse(d.toString())
-      if(r.error){
-        console.error('VK error:',r.error.error_msg)
+      if (r.error) {
+        console.error('VK error:', r.error.error_msg)
         process.exit()
-      }else{
+      } else {
         console.log(`Got it! Replacer: ${r.response.text}`)
         config.statusReplace = r.response.text
         loop()
       }
     })
   })
-}else{
+} else {
   loop()
 }
 
-function beforeExit(){
-  if(config.statusReplace && !isExiting){
+function beforeExit () {
+  if (config.statusReplace && !isExiting) {
     isExiting = true
     console.log('Setting replacer as status...')
-    https.get(`https://api.vk.com/method/status.set?text=${querystring.escape(config.statusReplace)}&access_token=${config.token}`, function(res){
+    https.get(`https://api.vk.com/method/status.set?text=${querystring.escape(config.statusReplace)}&access_token=${config.token}`, function (res) {
       res.on('data', (d) => {
         console.log('Done! Exiting...')
         process.exit()
       })
     })
-  }else{
+  } else {
     process.exit()
   }
 }
